5f7a7096548696b4ce513cd5eca05dbdb7f89a94
PhiPham <phipham681996@gmail.com>
2023-06-25 10:25:22 +0700

    fix: args

--- app/Services/ClassService.php
+++ app/Services/ClassService.php
@@ -8,13 +8,13 @@ use Arr;
 class ClassService
 {
 
-    public function getListClass($params, User $id)
+    public function getListClass($params, $user_id)
     {
         $limit = Arr::get($params, 'limit', config('pagination.limit'));
         $sort = Arr::get($params, 'sort', config('pagination.sort'));
         $order = Arr::get($params, 'order', config('pagination.order'));
         return User::join('class', 'users.id', 'class.user_id')
-            ->where('users.id', $id)
+            ->where('users.id', $user_id)
             ->orderBy($sort, $order)
             ->paginate($limit);
     }
